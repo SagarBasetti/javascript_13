b7771b417a8d1a330b78fa27a5ac0e4b7b1557d3
SagarBasetti <sagarbashetti@gmail.com>
2022-11-13 12:23:12 +0530

    ok

---
+++ test1.js
@@ -0,0 +1,6 @@
+let a = [10,20,30,40,50];
+let i=0;
+while (i<=a.length-1){
+    console.log(a[i]);
+    i++;
+}
---
+++ test2.js
@@ -0,0 +1,4 @@
+let b = ["abc", "def", "ghi"]
+for(let i = 0; i <= b.length-1; i++){
+    console.log(b[i]);
+}
