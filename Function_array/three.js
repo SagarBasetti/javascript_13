d0d1a19506ee57db7222b6415176ea963cc3a90b
SagarBasetti <sagarbashetti@gmail.com>
2022-11-20 12:26:51 +0530

    ok

---
+++ Function_array/three.js
@@ -0,0 +1,11 @@
+//let emp = { id:101, name:"xyz", company:"google"}
+//console.log(emp);
+
+//let emp={id:91, name:"nagaraj", native:"Murudeshwar"}
+//console.log(emp);
+
+let flifkart={Model:500,Price: 20000, Brand:"apple,"}
+
+let k= Object.values(flifkart)
+
+console.log(k);
